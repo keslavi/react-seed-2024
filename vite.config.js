b4df1f5c6b8220f41d119563c6d4f1d3875b7637
keslavi <dev@evensteven.us>
2025-05-02 15:57:07 -0400

    adjusted vitest.config.js to separate testing

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,9 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import path from "path";
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    globals:true,
-    environment:"jsdom",
-    setupFiles: './src/test/_setupTest.js',
-    css: true,
-    watch: true,
-    isolate: true, // <-- this helps with stale state
-    forceRerunTriggers: ['**/src/**/*.{js,jsx,ts,tsx}'], // triggers full rerun
-    //clearScreen: false // Explicitly prevents clearing 
-    fileParallelism: true, // default: true
-    poolOptions: {
-      threads: {
-        maxThreads: 8 // Adjust based on your CPU cores
-      },
-    },
-    onConsoleLog: (log, type) => {
-      // helpful for debugging
-      console.log(`[Vitest][${type}] ${log}`);
-    },
-    clearMocks: true,
-    restoreMocks: true,     
-  },
   server: {
     proxy: {
       //mock server should have the same endpoint as the eventual live endpoint
